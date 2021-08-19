283851419668403cecdaba340973182d4107f72a
KratiGupta <kratigupta8545@gmail.com>
2021-08-20 00:10:29 +0530

    Added welcome msg successfully

---
+++ NumberPlayListStreams.java
@@ -0,0 +1,13 @@
+package streamsjava;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberPlayListStreams {
+	
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Master Branch Of Java Streams");
+	}
+
+}
