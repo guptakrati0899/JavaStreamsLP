7c900aa2e0a9774a33019b4edd4c0e10b962e0bd
KratiGupta <kratigupta8545@gmail.com>
2021-08-20 01:33:29 +0530

    Added printing max and min even number

--- NumberPlayListStreams.java
+++ NumberPlayListStreams.java
@@ -2,10 +2,10 @@ package streamsjava;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class NumberPlayListStreams {
@@ -25,13 +25,23 @@ public class NumberPlayListStreams {
 		
 		Function<Integer,Double> toDoubleFunction = Integer::doubleValue;
 		Predicate<Integer> isEvenFunction = n -> n>0 && n%2 ==0;
-		
 		List<Double> doubleList = myNumberList.stream().filter(isEvenFunction).peek(n -> System.out.println("Peak Even Number : " +n)).map(toDoubleFunction).collect(Collectors.toList());
 		System.out.println("Printing Double List : " + doubleList);
 		
-		
+		//first find
 		Integer first = myNumberList.stream().filter(isEvenFunction).peek(n -> System.out.println("Peak Even Number : " +n)).findFirst().orElse(null);
 		System.out.println("First Even : " + first);
+		
+		
+		// MIN AND MAX
+		
+		Integer max = myNumberList.stream().filter(isEvenFunction).max(Comparator.comparing(Integer::intValue)).orElse(null);
+		System.out.println("Max Even : " +max);
+		
+		Integer min = myNumberList.stream().filter(isEvenFunction).min(Comparator.comparing(Integer::intValue)).orElse(null);
+		System.out.println("Min Even : " +min);
+		
+		
 	}
 
 }
